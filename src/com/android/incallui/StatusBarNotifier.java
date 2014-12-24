codeaurora-unoffical/platform-packages-apps-InCallUI

Voice Privacy Icon Support for CDMA
Voice privacy icons are added to InCallUI app.
When VP is enabled corresponding icon is displayed
on the UI.

Change-Id: Iadf3710f74987eac785f7816abc684d397a691b8
CRs-Fixed: 599389

src/com/android/incallui/StatusBarNotifier.java
@@ -451,10 +451,22 @@ public class StatusBarNotifier implements InCallPresenter.InCallStateListener {
         // different calls.  So if both lines are in use, display info
         // from the foreground call.  And if there's a ringing call,
         // display that regardless of the state of the other calls.
+        int resId;
+        int voicePrivacy = call.getCapabilities() & Call.Capabilities.VOICE_PRIVACY;
         if (call.getState() == Call.State.ONHOLD) {
-            return R.drawable.stat_sys_phone_call_on_hold;
+            if (voicePrivacy != 0) {
+                resId = R.drawable.stat_sys_vp_phone_call_on_hold;
+            } else {
+                resId = R.drawable.stat_sys_phone_call_on_hold;
+            }
+        } else {
+            if (voicePrivacy != 0) {
+                resId = R.drawable.stat_sys_vp_phone_call;
+            } else {
+                resId = R.drawable.stat_sys_phone_call;
+            }
         }
-        return R.drawable.stat_sys_phone_call;
+        return resId;
     }
 
     /**

DesolationRom-legacy/android_packages_apps_InCallUI

Conflicts:
        src/com/android/incallui/StatusBarNotifier.java

src/com/android/incallui/StatusBarNotifier.java
@@ -30,6 +30,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.os.Message;
 import android.telecom.PhoneAccount;
+import android.telecom.PhoneCapabilities;
 import android.text.TextUtils;
 
 import com.android.incallui.ContactInfoCache.ContactCacheEntry;
@@ -437,13 +438,25 @@ public class StatusBarNotifier implements InCallPresenter.InCallStateListener {
         // different calls.  So if both lines are in use, display info
         // from the foreground call.  And if there's a ringing call,
         // display that regardless of the state of the other calls.
+        int resId;
+        boolean voicePrivacy = call.can(PhoneCapabilities.VOICE_PRIVACY);
         if (call.getState() == Call.State.ONHOLD) {
-            return R.drawable.ic_phone_paused_white_24dp;
+            if (voicePrivacy) {
+                resId = R.drawable.stat_sys_vp_phone_call_on_hold;
+            } else {
+                resId =  R.drawable.ic_phone_paused_white_24dp;
+            }
         } else if (call.getSessionModificationState()
                 == Call.SessionModificationState.RECEIVED_UPGRADE_TO_VIDEO_REQUEST) {
-            return R.drawable.ic_videocam;
+            resId =  R.drawable.ic_videocam;
+        } else {
+            if (voicePrivacy) {
+                resId =  R.drawable.stat_sys_vp_phone_call;
+            } else {
+                resId =  R.drawable.ic_call_white_24dp;
+            }
         }
-        return R.drawable.ic_call_white_24dp;
+        return resId;
     }
 
     /**